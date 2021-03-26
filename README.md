da34e57fe6d4bf28721faaef76618ef38e473137
skamensky <shmuel.kamensky@shutterfly.com>
2021-03-26 18:07:46 +0300

    * Adding package.json to rsync list * Making restart script use sudo * Update dockerfile and docker start script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Server requires docker installed
+
+From the root of the directory run `sudo server/restart_server.sh` to run the server
--- server/restart_server.sh
+++ server/restart_server.sh
@@ -5,8 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 IMAGE_NAME=feel_tracker_server
 cd $DIR
+
+# run build from parent directory so docker's COPY includes the dist directory
+cd ..
 # kill all running instances of our image
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=$IMAGE_NAME --format="{{.ID}}")) || true
 
-docker build -t $IMAGE_NAME .
-docker run -p 80:3000 -v "$(dirname $(pwd))":/app -d $IMAGE_NAME
+docker build -t $IMAGE_NAME  -f server/Dockerfile .
+docker run -p 80:8080 -v "$(dirname $(pwd))/persistent_data":/persistent_data -d $IMAGE_NAME
